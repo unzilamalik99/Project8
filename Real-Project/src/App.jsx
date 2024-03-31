1346eeb8f3c1a796599b90f8b027ea930a4d9be4
unzilamalik99 <https://github.com/unzilamalik99/website>
2024-03-31 20:22:44 +0500

    first commit

---
+++ Real-Project/src/App.jsx
@@ -0,0 +1,9 @@
+import "./App.css";
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-white">Hello world!</h1>
+      <h1>hello</h1>
+    </div>
+  );
+}
